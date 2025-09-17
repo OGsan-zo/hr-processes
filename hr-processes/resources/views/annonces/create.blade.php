cbe681c5bf8521a00e91b7b22f591f5d86c14f63
Antonio2772 <atn.randr@gmail.com>
2025-09-17 10:10:00 +0300

    feat: add views for listing, creating and editing annonces

---
+++ hr-processes/resources/views/annonces/create.blade.php
@@ -0,0 +1,17 @@
+<h1>Nouvelle annonce</h1>
+<form action="{{ route('annonces.store') }}" method="POST">
+    @csrf
+    <label>Titre :</label>
+    <input type="text" name="titre" required><br><br>
+
+    <label>Description :</label>
+    <textarea name="description" required></textarea><br><br>
+
+    <label>Date début :</label>
+    <input type="date" name="date_debut"><br><br>
+
+    <label>Date fin :</label>
+    <input type="date" name="date_fin"><br><br>
+
+    <button type="submit">Publier</button>
+</form>
