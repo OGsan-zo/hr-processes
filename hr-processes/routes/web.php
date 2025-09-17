c4357873d097e7e79e4aebabc83d90e1fd74a5d8
Antonio2772 <atn.randr@gmail.com>
2025-09-17 09:39:38 +0300

    chore: register candidat routes

--- hr-processes/routes/web.php
+++ hr-processes/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CandidatController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/candidats/create', [CandidatController::class, 'create'])->name('candidats.create');
+Route::post('/candidats', [CandidatController::class, 'store'])->name('candidats.store');
